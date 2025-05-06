d145710407414d8b17012bc9c9c01af0a2a21ae5
Giorgio Abelli <giorgio.abelli99@gmail.com>
2025-05-06 12:17:40 +0200

    feat: use resolveToCall to retrieve signature

--- app/src/main/kotlin/org/kotlinlsp/actions/Hover.kt
+++ app/src/main/kotlin/org/kotlinlsp/actions/Hover.kt
@@ -1,20 +1,19 @@
 package org.kotlinlsp.actions
 
-import com.intellij.lang.jvm.JvmClassKind
-import com.intellij.lang.jvm.JvmModifier
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiMethod
-import com.intellij.psi.PsiNamedElement
-import com.intellij.psi.PsiVariable
 import com.intellij.psi.util.parentOfType
 import org.eclipse.lsp4j.Position
 import org.eclipse.lsp4j.Range
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.renderer.declarations.impl.KaDeclarationRendererForSource
+import org.jetbrains.kotlin.analysis.api.resolution.successfulConstructorCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.successfulFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.successfulVariableAccessCall
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtReferenceExpression
 import org.kotlinlsp.common.getElementRange
 import org.kotlinlsp.common.toOffset
 
@@ -27,74 +26,22 @@ fun hoverAction(ktFile: KtFile, position: Position): Pair<String, Range>? {
     val ktElement = ktFile.findElementAt(offset)?.parentOfType<KtElement>() ?: return null
     val range = getElementRange(ktFile, ktElement)
 
-    val ktDeclaration =
-        (ktElement as? KtDeclaration)
-            ?: (ktFile.findReferenceAt(offset)?.resolve() as? KtDeclaration)
-    if (ktDeclaration != null) {
-        val text = analyze(ktDeclaration) {
-            ktDeclaration.symbol.render(renderer)
-        }
-        return Pair(text, range)
-    }
-
-    val javaDeclaration =
-        (ktFile.findReferenceAt(offset)?.resolve() as? PsiNamedElement)
-            ?: return null
-    val text = when (javaDeclaration) {
-        is PsiMethod -> buildString {
-            append("fun ")
-
-            javaDeclaration.typeParameters.takeIf { it.isNotEmpty() }?.let {
-                append("<")
-                append(it.joinToString(", ") { it.name.toString() })
-                append("> ")
+    val text =
+        (ktElement as? KtDeclaration ?: ktFile.findReferenceAt(offset)?.resolve() as? KtDeclaration)?.let {
+            analyze(it) {
+                it.symbol.render(renderer)
             }
-
-            append(javaDeclaration.name)
-            append("(")
-            append(javaDeclaration.parameterList.parameters.joinToString(", ") {
-                "${it.name}: ${it.type.presentableText}"
-            })
-            append(")")
-
-            javaDeclaration.returnType?.takeIf { it.presentableText != "void" }?.let {
-                append(": ${it.presentableText}")
+        } ?: ktElement.parentOfType<KtReferenceExpression>()?.let {
+            analyze(it) {
+                val call = it.resolveToCall()
+                val successfulCall =
+                    call?.successfulFunctionCallOrNull()
+                        ?: call?.successfulConstructorCallOrNull()
+                        ?: call?.successfulVariableAccessCall()
+                        ?: return null
+                val symbol = successfulCall.symbol
+                symbol.render(renderer)
             }
-        }
-
-        is PsiClass -> buildString {
-            when (javaDeclaration.classKind) {
-                JvmClassKind.CLASS -> append("class ")
-                JvmClassKind.INTERFACE -> append("interface ")
-                JvmClassKind.ANNOTATION -> append("annotation class ")
-                JvmClassKind.ENUM -> append("enum class ")
-            }
-            append(javaDeclaration.name)
-
-            javaDeclaration.typeParameters.takeIf { it.isNotEmpty() }?.let {
-                append("<")
-                append(it.joinToString(", ") { it.name.toString() })
-                append(">")
-            }
-
-            javaDeclaration.extendsList?.referencedTypes?.takeIf { it.isNotEmpty() }?.let {
-                append(" : ")
-                append(it.joinToString(", ") { it.presentableText })
-            }
-        }
-
-        is PsiVariable -> buildString {
-            if (javaDeclaration.hasModifier(JvmModifier.FINAL)) {
-                append("val ")
-            } else {
-                append("var ")
-            }
-            append(javaDeclaration.name)
-            append(": ")
-            append(javaDeclaration.type.presentableText)
-        }
-
-        else -> return null
-    }
+        } ?: return null
     return Pair(text, range)
 }
